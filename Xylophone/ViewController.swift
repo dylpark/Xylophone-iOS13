afb80bb8c076bede0bbc105abd8ca0f789be2752
dylpark <dylanpark.design@gmail.com>
2021-01-22 12:04:27 +1000

    Update ViewController.swift

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -23,6 +23,15 @@ class ViewController: UIViewController {
         
         playSound(soundName: sender.currentTitle!)
         
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code shold execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring senders opactity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
     }
     
     
